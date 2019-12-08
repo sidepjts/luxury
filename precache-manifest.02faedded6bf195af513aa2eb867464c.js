8e1d96c5a4972b55b141d24318405224f1e3fbe7
carlobutelli <dev.butelli@gmail.com>
2019-12-08 18:47:30 +0100

    Updates

---
+++ precache-manifest.02faedded6bf195af513aa2eb867464c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa96aa6bc1abbe00c34b33484c27519",
+    "url": "/luxury/index.html"
+  },
+  {
+    "revision": "5fd2849f337affd1b2b8",
+    "url": "/luxury/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "821bdb6676a878c5e218",
+    "url": "/luxury/static/css/main.55a580f8.chunk.css"
+  },
+  {
+    "revision": "5fd2849f337affd1b2b8",
+    "url": "/luxury/static/js/2.0331ebdb.chunk.js"
+  },
+  {
+    "revision": "821bdb6676a878c5e218",
+    "url": "/luxury/static/js/main.99a36ef9.chunk.js"
+  },
+  {
+    "revision": "7c91a10a3a51f36fe11c",
+    "url": "/luxury/static/js/runtime-main.e19bd400.js"
+  }
+]);
